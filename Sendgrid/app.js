14c4b57c2a2343cc7fb8bef363a2274ad3583cea
derek-byte <derek.s.prog@gmail.com>
2022-05-14 17:20:20 -0400

    Sendgrid Twilio Testing

---
+++ Sendgrid/app.js
@@ -0,0 +1,18 @@
+const sgMail = require("@sendgrid/mail")
+
+const API_KEY = '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'
+
+sgMail.setApiKey(API_KEY)
+
+const message = {
+    to: 'derek.s.prog@gmail.com',
+    from: 'CookitModel@gmail.com',
+    subject: 'Sending with Twilio SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+
+sgMail
+    .send(message)
+    .then(response => console.log('Email sent...'))
+    .catch(error => console.log(error.message))
